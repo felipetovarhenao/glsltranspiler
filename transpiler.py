12c18c62bd1797ce6b0c2bc1914e300ad906155d
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-10-20 15:09:20 -0400

    Add GL_ES def removal method

--- transpiler.py
+++ transpiler.py
@@ -7,8 +7,30 @@ class GLTranspiler:
     def to_shadertoy(self, input_file: str, output_file: str):
         lines = self.__resolve_include(os.path.abspath(input_file))
         code = self.__replace_multiple_line_breaks("".join(lines))
+        code = self.__remove_precision_def(code)
         with open(output_file, 'w') as f:
             f.write(code)
+
+    def __remove_precision_def(self, input_string: str):
+        def condition_function(s: str):
+            return "GL_ES" in s
+        lines = input_string.split('\n')
+        result_lines = []
+        inside_ifdef = False
+
+        for line in lines:
+            if line.strip().startswith("#ifdef"):
+                inside_ifdef = condition_function(line.strip()[7:])
+                if inside_ifdef:
+                    continue
+            elif line.strip().startswith("#endif"):
+                inside_ifdef = False
+                continue
+
+            if not inside_ifdef:
+                result_lines.append(line)
+
+        return '\n'.join(result_lines)
 
     def __get_include_path(self, include_statement):
         pattern = r'#include "(.*)"'
